7b51bffb9685344eb75d332d57ac010e0cfe7661
guuTASA <hdusjm@163.com>
2018-07-10 17:48:33 +0800

    add handlers.py

---
+++ www/handlers.py
@@ -0,0 +1,19 @@
+
+
+def get(path):
+
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kw)
+			return func(*args, **kw)
+		wrapper.__method__ = "GET"
+		wrapper.__route__ = path
+		return wrapper
+	return docorator
+
+
+
+
+
+
+
