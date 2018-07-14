215af42a3ab7175e21bc69a963b67c4fc48518bf
guuTASA <hdusjm@163.com>
2018-07-14 21:24:25 +0800

    add configs

---
+++ www/config_dafault.py
@@ -0,0 +1,15 @@
+# config_dafault.py
+
+configs = {
+	'debug':True,
+	'db':{
+		'host':'127.0.0.1',
+		'port':3306,
+		'user':'root',
+		'password':'123456',
+		'database':'test'
+	},
+	'session'{
+		'secret':'AwEsOmE'
+	}
+}
