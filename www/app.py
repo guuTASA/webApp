f7aac73afff3266b8de3dec859ab3f34d748f15c
guuTASA <hdusjm@163.com>
2018-07-17 19:50:07 +0800

    back-end complete!

--- www/app.py
+++ www/app.py
@@ -73,12 +73,12 @@ async def response_factory(app, handler):
 		if isinstance(r, dict):
 			template = r.get('__template__')
 			if template is None:
-				resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode(utf-8))
+				resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
 				resp.content_type = 'application/json;charset=utf-8'
 				return resp
 			else:
-				resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode(utf-8))
-				reso.content_type = 'text/html;charest-utf-8'
+				resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+				resp.content_type = 'text/html;charest-utf-8'
 				return resp
 		if isinstance(r, int) and r >= 100 and r <600:
 			return web.Response(r)
--- www/handlers.py
+++ www/handlers.py
@@ -10,15 +10,27 @@ from models import User, Comment, Blog, next_id
 
 
 
-# TEST1
+# TEST 1
+
+# @get('/')
+# async def index(request):
+# 	users = await User.findAll()
+# 	return{
+# 		'__template__':'test.html',
+# 		'users':users
+# 	}
+
+# TEST 2
 @get('/')
 async def index(request):
-	users = await User.findAll()
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+		Blog(id=1, name='Test Blog1', summary=summary, created_at=time.time()-120),
+		Blog(id=2, name='Test Blog2', summary=summary, created_at=time.time()-3600),
+		Blog(id=3, name='Test Blog3', summary=summary, created_at=time.time()-640000)
+	]
 	return{
-		'__template__':'test.html',
-		'users':users
+		'__template__':'blogs.html',
+		'blogs':blogs
 	}
-
-
-
 
